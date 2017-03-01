ac794016e6ac82c68dca988d1174ca2960d64709
Michael Gaudet <Michael.G224@gmail.com>
2017-03-01 15:57:34 -0600

    fixxed footer bug on smaller screens and fixed results page on load

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,19 +1,22 @@
-angular.module('nytApp').controller('mainCtrl',function($scope,$state, mainSrvc){
-    $scope.getArticles = function(term){
-        if(term === undefined){
+angular.module('nytApp').controller('mainCtrl', function($scope, $state, mainSrvc) {
+    var init = true;
+    $scope.getArticles = function(term) {
+        if (term === undefined) {
             return
         }
-        mainSrvc.getArticles(term).then(function(response){
+        mainSrvc.getArticles(term).then(function(response) {
             console.log(response.data.response.docs);
             $scope.articles = response.data.response.docs
             console.log(moment($scope.articles[0].pub_date).fromNow());
-            $scope.articles.map(function(x){
+            $scope.articles.map(function(x) {
                 x.pub_date = moment(x.pub_date).fromNow();
             })
-            $state.go('results')
+            if (!init) {
+                $state.go('results')
+            }
         })
     }
-    mainSrvc.initArticles().then(function(response){
+    mainSrvc.initArticles().then(function(response) {
         console.log(response.data.results);
         $scope.popArts = response.data.results
     })
